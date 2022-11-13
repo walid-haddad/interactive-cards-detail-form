75148f9212034c121604674d8e584a6f130e4e63
cluxear <cluxear@gmail.com>
2022-11-13 20:18:46 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateform(){  
+    console.log("test");
+    const name=document.myform.name.value;  
+      
+    if (name==null || name==""){  
+      alert("Name can't be blank");  
+      return false;  
+    }else if(password.length<6){  
+      alert("Password must be at least 6 characters long.");  
+      return false;  
+      }  
+    }  
